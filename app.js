905beffd6c7ff5cd0c7b0771c859962fd63b8bfe
vccolombo <victorcora98@gmail.com>
2019-09-26 23:10:57 -0300

    Create commands for notes app

---
+++ app.js
@@ -0,0 +1,54 @@
+const chalk = require('chalk');
+const yargs = require('yargs');
+const getNotes = require('./notes');
+
+// Create note command
+yargs.command({
+    command: 'add',
+    describe: 'Create a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function createNote(argv) {
+        console.log('Title: ' + argv.title);
+        console.log('Body: ' + argv.body);
+    }
+});
+
+// Remove note command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function() {
+        console.log('Removing a note');
+    }
+});
+
+// List all notes command
+yargs.command({
+    command: 'list',
+    describe: 'List all notes',
+    handler: function() {
+        console.log('Listing all notes');
+    }
+});
+
+// Read note command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function() {
+        console.log('Reading a note');
+    }
+});
+
+yargs.parse()
